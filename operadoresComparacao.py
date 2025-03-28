49313c731a8d7cd61aaa593bc349bb56e874e9f6
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2025-03-28 10:55:22 -0300

    operadores comparação

---
+++ operadoresComparacao.py
@@ -0,0 +1,22 @@
+
+x = 10
+y = 10
+
+# Comparaçao de igualdade
+print(x == y)
+
+# Diferença
+print(x != y)
+
+# Superioridade (>)
+print(x > y)
+
+# Inferioridade (<)
+print(x < y)
+
+# Maior ou igual (>=)
+print(x >= y)
+
+# Menor ou igual (<=)
+print(x <= y)
+
